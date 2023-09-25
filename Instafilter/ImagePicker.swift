988194fb3d62a8de84ed50893f85d8759aa7b32d
Maria Sandu <Maria.Sandu@emids.com>
2023-09-25 17:33:07 +0300

    Load image, UIKit helper

---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,23 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Maria Sandu on 25.09.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+
+    }
+}
---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Maria Sandu on 25.09.2023.
+//
+
+import SwiftUI
+
+struct ImageSaver: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageSaver()
+}
